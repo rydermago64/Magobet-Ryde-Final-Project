6d85a0acbf2bc1a88192924b466d0ca50b936bd6
rydermago64 <ryder.magobet@yahoo.com>
2023-12-01 13:03:52 -0800

    test push

---
+++ main.py
@@ -0,0 +1,8 @@
+# This file was created by: Ryder Magobet
+
+# passionate about cars, and racing
+# make racing game
+# Final Project Name: Cuttin' up
+# Goals:
+# move, drive fast, cut up, cool colors, turns, extoic cars, paved roads...etc.
+# oiewfineoneodknlknedv
